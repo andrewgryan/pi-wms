f76ddf144fcda80a053d8556c48e0efc17a7cc5e
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-04-10 09:22:08 +0100

    hello, world memcache app

--- blog/server.py
+++ blog/server.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from typing import Annotated
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from pymemcache.client import base
+from fastapi.templating import Jinja2Templates
+
+client = base.Client(("cache", 11211))
 
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="./www/static"), name="static") 
 
-@app.get("/")
-async def index():
-    return FileResponse("./www/index.html")
+templates = Jinja2Templates(directory="./www")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    name = client.get("name", default=b"World").decode("utf-8")
+    return templates.TemplateResponse(request=request, name="index.html", context={"name": name})
+
+@app.post("/signin")
+async def signin(name: Annotated[str, Form()]):
+    client.set("name", name)
+    return {"name": name}
