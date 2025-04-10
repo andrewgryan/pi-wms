c0972f73ee5e1fe5ee65f10f2f75ce2e1ab5a48e
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-04-11 00:24:36 +0100

    view transitions and hypermedia forms

--- blog/server.py
+++ blog/server.py
@@ -1,6 +1,6 @@
 from typing import Annotated
-from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Form, Request, Response
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from pymemcache.client import base
 from fastapi.templating import Jinja2Templates
@@ -14,12 +14,25 @@ app.mount("/static", StaticFiles(directory="./www/static"), name="static")
 
 templates = Jinja2Templates(directory="./www")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     name = client.get("name", default=b"World").decode("utf-8")
     return templates.TemplateResponse(request=request, name="index.html", context={"name": name})
 
+
+@app.get("/about", response_class=HTMLResponse)
+async def about(request: Request):
+    return FileResponse("./www/about.html")
+
+
 @app.post("/signin")
 async def signin(name: Annotated[str, Form()]):
+    name = name.strip()
     client.set("name", name)
-    return {"name": name}
+    headers = {
+        "Content-type": "text/html;charset=UTF-8",
+        "Content-Length": "0",
+        "Location": "/"
+    }
+    return Response(status_code=303, headers=headers, content="")
