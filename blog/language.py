badf007ca77257b1af394a851f626c97afeaa008
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-04-11 20:53:28 +0100

    simple calculator

--- blog/language.py
+++ blog/language.py
@@ -1,2 +1,28 @@
+import io
+
+
 def interpret(code: str) -> str:
-    return code
+    result = 0
+    stream = io.StringIO(code)
+    operator = "+"
+    while True:
+        token = stream.read(1)
+        if token == '':
+            break
+        elif token == ' ':
+            continue
+        elif token.isdigit():
+            number = 0
+            while token.isdigit():
+                number *= 10
+                number += int(token)
+                token = stream.read(1)
+            if operator == "+":
+                result += number
+            elif operator == "*":
+                result *= number
+        elif token == "+":
+            operator = "+"
+        elif token == "*":
+            operator = "*"
+    return str(result)
--- blog/test_language.py
+++ blog/test_language.py
@@ -1,2 +1,12 @@
-def test_pass():
-    assert True
+import pytest
+import language
+
+
+@pytest.mark.parametrize("code,result", [
+    ("2 + 2", "4"),
+    ("2 + 3", "5"),
+    ("10 + 1", "11"),
+    ("10 * 2", "20"),
+])
+def test_interpret(code, result):
+    assert language.interpret(code) == result
