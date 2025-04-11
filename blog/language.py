6d6b16f719b4f4c98453a7db1ce15220a0133362
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-04-11 21:34:21 +0100

    work in progress

--- blog/language.py
+++ blog/language.py
@@ -6,23 +6,39 @@ def interpret(code: str) -> str:
     stream = io.StringIO(code)
     operator = "+"
     while True:
-        token = stream.read(1)
+        token = peek(stream)
         if token == '':
             break
         elif token == ' ':
+            stream.read(1)
             continue
         elif token.isdigit():
-            number = 0
-            while token.isdigit():
-                number *= 10
-                number += int(token)
-                token = stream.read(1)
+            number = parse_number(stream)
             if operator == "+":
                 result += number
+            elif operator == "-":
+                result -= number
             elif operator == "*":
                 result *= number
-        elif token == "+":
-            operator = "+"
-        elif token == "*":
-            operator = "*"
+        elif token in ("+", "*", "-"):
+            operator = stream.read(1)
+        else:
+            raise Exception(f"Unknown token: {token}")
     return str(result)
+
+
+def parse_number(stream):
+    number = 0
+    while peek(stream).isdigit():
+        token = stream.read(1)
+        number *= 10
+        number += int(token)
+    return number
+
+
+def peek(stream):
+    """look ahead by 1 token"""
+    cur = stream.tell()
+    token = stream.read(1)
+    stream.seek(cur)
+    return token
--- blog/test_language.py
+++ blog/test_language.py
@@ -7,6 +7,10 @@ import language
     ("2 + 3", "5"),
     ("10 + 1", "11"),
     ("10 * 2", "20"),
+    ("1 + 2 + 3 + 4 + 5", "15"),
+    ("1*1", "1"),
+    ("1-1", "0"),
+    # ("1 + 2 * 3", "7"),
 ])
 def test_interpret(code, result):
     assert language.interpret(code) == result
