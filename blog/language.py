c57fe59a3c09ebb3598fcf561db767764cf23efe
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-04-12 11:30:07 +0100

    extend language arithmetic support

--- blog/language.py
+++ blog/language.py
@@ -20,7 +20,12 @@ def interpret(code: str) -> str:
                 result -= number
             elif operator == "*":
                 result *= number
-        elif token in ("+", "*", "-"):
+            elif operator == "/":
+                try:
+                    result /= number
+                except ZeroDivisionError:
+                    return {"status": "error", "message": f"ZeroDivisionError: division by {number}"}
+        elif token in ("+", "*", "-", "/"):
             operator = stream.read(1)
         else:
             return {"status": "error", "message": f"Unknown token: '{token}' at position {stream.tell()}"}
@@ -28,11 +33,30 @@ def interpret(code: str) -> str:
 
 
 def parse_number(stream):
+    whole = parse_int(stream)
+    if peek(stream) == ".":
+        stream.read(1)
+        decimals = parse_decimals(stream)
+        return whole + decimals
+    else:
+        return whole
+
+
+def parse_int(stream):
     number = 0
     while peek(stream).isdigit():
         token = stream.read(1)
         number *= 10
         number += int(token)
+    return number
+
+def parse_decimals(stream):
+    number = 0.0
+    factor = 0.1
+    while peek(stream).isdigit():
+        token = stream.read(1)
+        number += int(token) * factor
+        factor *= 0.1
     return number
 
 
--- blog/test_language.py
+++ blog/test_language.py
@@ -3,14 +3,17 @@ import language
 
 
 @pytest.mark.parametrize("code,result", [
+    ("1.0", "1.0"),
+    ("3.14", "3.14"),
     ("2 + 2", "4"),
     ("2 + 3", "5"),
     ("10 + 1", "11"),
     ("10 * 2", "20"),
     ("1 + 2 + 3 + 4 + 5", "15"),
     ("1*1", "1"),
-    ("1-1", "0"),
+    ("1/1", "1.0"),
+    ("1/0", "ZeroDivisionError: division by 0"),
     # ("1 + 2 * 3", "7"),
 ])
 def test_interpret(code, result):
-    assert language.interpret(code) == result
+    assert language.interpret(code)["message"] == result
